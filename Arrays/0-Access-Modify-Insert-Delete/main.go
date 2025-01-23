1019c6c56dd3c7086a958ca09b6ef1bb92029197
Ehioje Henry Erabor <ehiojee@yahoo.com>
2025-01-23 23:59:36 +0000

    add more info for time complexity

--- Arrays/0-Access-Modify-Insert-Delete/main.go
+++ Arrays/0-Access-Modify-Insert-Delete/main.go
@@ -13,7 +13,7 @@ func main() {
 	// Accessing an element ... Time complexity : O(1), because if you know the index you will get the element
 	fmt.Printf("Element at index 2 is %v\n", arr[2])
 
-	// Accessing the elements of an array using for loop
+	// Accessing the elements of an array using for loop ... Time complexity : O(n) because if you have n elements the loop will run n times to access each element
 	fmt.Printf("Accessing all elements in %v:\n", arr)
 	for x, y := range arr {
 		fmt.Printf("Element at index %d is %v\n", x, y)
