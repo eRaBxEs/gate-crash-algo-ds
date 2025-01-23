9c411a1c5dc4b7d9a4ed9ff6e7a4afdbcce58061
Ehioje Henry Erabor <ehiojee@yahoo.com>
2025-01-23 23:40:06 +0000

    printing an array/slice

---
+++ Arrays/0-Access-Modify-Insert-Delete/main.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	// Declaration and initialization of slice:array
+	arr := []int{1, 2, 3, 4, 5}
+	fmt.Printf("Printing the array: %v\n", arr)
+	fmt.Printf("Printing more details of the given array: %#v\n", arr)
+
+}
