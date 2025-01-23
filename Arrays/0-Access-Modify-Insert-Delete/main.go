59193cfe4e8c593c4d66563d7382f05cf4b616f6
Ehioje Henry Erabor <ehiojee@yahoo.com>
2025-01-23 23:51:24 +0000

    accessing elements in array

--- Arrays/0-Access-Modify-Insert-Delete/main.go
+++ Arrays/0-Access-Modify-Insert-Delete/main.go
@@ -10,4 +10,13 @@ func main() {
 	fmt.Printf("Printing the array: %v\n", arr)
 	fmt.Printf("Printing more details of the given array: %#v\n", arr)
 
+	// Accessing an element ... Time complexity : O(1), because if you know the index you will get the element
+	fmt.Printf("Element at index 2 is %v\n", arr[2])
+
+	// Accessing the elements of an array using for loop
+	fmt.Printf("Accessing all elements in %v:\n", arr)
+	for x, y := range arr {
+		fmt.Printf("Element at index %d is %v\n", x, y)
+	}
+
 }
